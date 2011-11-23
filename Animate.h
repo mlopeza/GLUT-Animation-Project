6cdd9ea31f218f31e94f71ef32ee1d05d7714742
Mario Lopez <mario.aleman@hotmail.com>
2011-11-23 14:42:39 -0600

    Bug in Animated.h

--- Animate.h
+++ Animate.h
@@ -19,12 +19,13 @@ void animateModel(int id){
 	Elemento *nodo;	
 	for(int i=0;i<numAnimatedNodes;i++){
 		nodo = seleccionaElemento(raiz,animatedNodes[i]);
+		if(i == 1)
+		printf("%s %d %d %d\n",animatedNodes[i],aniMatrix[id][i][0],aniMatrix[id][i][1],aniMatrix[id][i][2]);
 		nodo->theta[0] = aniMatrix[id][i][0];
 		nodo->theta[1] = aniMatrix[id][i][1];
 		nodo->theta[2] = aniMatrix[id][i][2];
 	
 	}
-	printf("%d\n",id);
 	glutTimerFunc(speed,animateModel,id+direccion);
 
 }
@@ -44,6 +45,7 @@ void animation(int id){
 }
 
 void leeAnimacion(char *direccion){
+	
 	printf("Hola\n");
 	char buffer[1024],trash[2];
 
@@ -52,25 +54,25 @@ void leeAnimacion(char *direccion){
 		fprintf(stderr,"No se pudo abrir el archivo %s\n",direccion);
 		return;
 	}
-	
+
 	while(!feof(file)){
 		fgets(buffer,1024,file);
 		sscanf(buffer,"%d %d",&numAnimatedNodes,&keyFrames);
-		printf("Buffer 6: %s",buffer);
-
+		printf("Buffer 6: %d %d",numAnimatedNodes,keyFrames);
+		
+		
 		for(int i =0; i<numAnimatedNodes && !feof(file);i++){
 			animatedNodes[i]=(char *) malloc(6*sizeof(char));
 			fgets(buffer,1024,file);
-			sscanf(buffer,"%s",animatedNodes[i]);
-			printf("Buffer %d: %s",i,buffer);
+			sscanf(buffer,"%s [^\n]",animatedNodes[i]);
 		}
 
 
 		int x = 0;
-		for(int i =0;i<keyFrames;i++){
-			for(int j=0;j<numAnimatedNodes;j++){
+		for(int i =0;i<numAnimatedNodes;i++){
+			for(int j=0;j<keyFrames;j++){
 				fgets(buffer,1024,file);
-				sscanf(buffer,"%d %d %d",&aniMatrix[i][j][0],&aniMatrix[i][j][1],&aniMatrix[i][j][2]);
+				sscanf(buffer,"%d %d %d [^\n]",&aniMatrix[j][i][0],&aniMatrix[j][i][1],&aniMatrix[j][i][2]);
 				x++;
 			}
 		
@@ -78,15 +80,14 @@ void leeAnimacion(char *direccion){
 		printf("X:%d\n",x);
 		break;
 	}
-	for(int i =0;i<keyFrames;i++){
-		for(int j=0;j<numAnimatedNodes;j++){
-		printf("%d %d %d\n",aniMatrix[i][j][0],aniMatrix[i][j][1],aniMatrix[i][j][2]);
+	for(int i =0;i<numAnimatedNodes;i++){
+		for(int j=1;j<keyFrames;j++){
+			printf("%d %d %d\n",aniMatrix[j][i][0],aniMatrix[j][i][1],aniMatrix[j][i][2]);
 		}
 	}
 	printf("%d\n",keyFrames);
 	animateMatrix = 1;
-	glutTimerFunc(1000,animateModel,0);
-
+	glutTimerFunc(1000,animateModel,0);		
 
 }
 
